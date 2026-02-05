560640e4f7f7600bec8ebe33c72488edf439a3b6
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2026-02-04 23:09:56 -0800

    Add test for merging contexts that bind the same input to the same action

--- tests/test_context_mapping_combine.gd
+++ tests/test_context_mapping_combine.gd
@@ -3,12 +3,14 @@ extends GUIDETestBase
 var _context1: GUIDEMappingContext
 var _context2: GUIDEMappingContext
 var _action: GUIDEAction
+var _action2d: GUIDEAction
 
 
 func _setup() -> void:
 	_context1 = mapping_context()
 	_context2 = mapping_context()
 	_action = action_bool("some_action")
+	_action2d = action_2d("some_action")
 
 
 func test_inputs_are_combined() -> void:
@@ -44,3 +46,36 @@ func test_inputs_are_combined() -> void:
 	
 	# check that the trigger stopped
 	await assert_not_triggered(_action)
+
+func test_merging_same_input() -> void:
+	var input1 := input_key(KEY_A)
+	var swizzle := modifier_input_swizzle()
+	map(_context1, _action2d, input1, [])
+	map(_context2, _action2d, input1, [swizzle])
+	
+	# when i enable two mapping contexts with the same action
+	# both inputs should trigger the action
+	GUIDE.enable_mapping_context(_context1)
+	GUIDE.enable_mapping_context(_context2)
+
+	# when i press the key
+	await key_down(KEY_A)
+	
+	# then the action is triggered
+	await assert_triggered(_action2d)
+	assert_axis_2d(_action2d, Vector2(1, 0), Vector2(0.001, 0))
+	
+	await key_up(KEY_A)
+	
+	# when i disable mapping context 1
+	GUIDE.disable_mapping_context(_context1)
+	
+	# and i press the key
+	await key_down(KEY_A)
+	
+	# then the action is triggered
+	await assert_triggered(_action2d)
+	# and the vector is swizzled according to mapping context 2
+	assert_axis_2d(_action2d, Vector2(0, 1), Vector2(0, 0.001))
+	
+	await key_up(KEY_A)
