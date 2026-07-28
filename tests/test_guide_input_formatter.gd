godotneers/G.U.I.D.E

commit c9c63a23e87b10cb6ca97329d387d65b498597f1

fix: make input formatter properly handle incomplete chords on input type filtering
fixes #175

addons/guide/ui/guide_input_formatter.gd
@@ -189,6 +189,7 @@ func _materialized_as_text(input:MaterializedInput) -> String:
 ## Renders materialized input as rich text.
 func _materialized_as_richtext_async(input:MaterializedInput) -> String:
 	_ensure_readiness()	
+	print(input)
 	if input is MaterializedSimpleInput:
 		var icon:Texture2D = null
 		for renderer:GUIDEIconRenderer in _icon_renderers:
@@ -269,10 +270,12 @@ func _materialize_action_input(action:GUIDEAction) -> MaterializedInput:
 				chord.parts.append(combo)
 			if combos.is_empty():
 				if input_mapping.input != null:
-					chord.parts.append(
-						_materialize_input(FormattingContext.for_action(input_mapping.input, input_mapping, action))
-					)					
-			result.parts.append(chord)
+					var additional_inputs := _materialize_input(FormattingContext.for_action(input_mapping.input, input_mapping, action))
+					# additional inputs can be blank if they are filtered out. if they are blank
+					# we can discard the whole input mapping, as it could never trigger (we have a chorded action + no input)
+					if not additional_inputs.is_blank():
+						chord.parts.append(additional_inputs)					
+						result.parts.append(chord)
 		else:
 			for combo in combos:
 				result.parts.append(combo)
@@ -333,7 +336,8 @@ func _materialize_input(context:FormattingContext, materialize_actions:bool = tr
 	
 	
 class MaterializedInput:
-	pass
+	func is_blank() -> bool:
+		return false
 	
 class MaterializedSimpleInput:
 	extends MaterializedInput
@@ -341,18 +345,40 @@ class MaterializedSimpleInput:
 	
 	func _init(input:GUIDEInput):
 		self.input = input
+		
+	func _to_string() -> String:
+		return "MaterializedSimpleInput(%s)" % input
 	
 class MaterializedMixedInput:
 	extends MaterializedInput
 	var parts:Array[MaterializedInput] = []
 	
+	func is_blank() -> bool:
+		return parts.is_empty()
+	
+	func _to_string() -> String:
+		return "MaterializedMixedInput(%s)" % ", ".join(parts.map(func(it:MaterializedInput)->String: return it.to_string()))
+	
 class MaterializedChordedInput:
 	extends MaterializedInput
 	var parts:Array[MaterializedInput] = []
+
+	func is_blank() -> bool:
+		return parts.is_empty()
+
+	func _to_string() -> String:
+		return "MaterializedChordedInput(%s)" % ", ".join(parts.map(func(it:MaterializedInput)->String: return it.to_string()))
 	
 class MaterializedComboInput:
 	extends MaterializedInput
 	var parts:Array[MaterializedInput] = []
+
+	func is_blank() -> bool:
+		return parts.is_empty()
+
+	func _to_string() -> String:
+		return "MaterializedComboInput(%s)" % ", ".join(parts.map(func(it:MaterializedInput)->String: return it.to_string()))
+	
 
 ## A formatting context.
 class FormattingContext:

tests/test_guide_input_formatter.gd
@@ -1,0 +1,126 @@
+# Tests the input formatter.
+extends GUIDETestBase
+
+# we make a high priority default provider so it doesn't matter which device
+# is currently connected, we always get the default strings.	
+class DefaultProvider:
+	extends GUIDEInputFormatter.DefaultTextProvider
+	
+	func _init() -> void:
+		super()
+		priority = -100	
+	
+var _context:GUIDEMappingContext
+var _action:GUIDEAction
+var _formatter:GUIDEInputFormatter
+
+
+func _setup() -> void:
+	_formatter = GUIDEInputFormatter.for_active_contexts()
+	GUIDEInputFormatter.add_text_provider(DefaultProvider.new())
+	_context = mapping_context()
+	_action = action_bool()	
+	
+	
+func _filter_keyboard() -> void:
+	_formatter.formatting_options.input_filter = func(context:GUIDEInputFormatter.FormattingContext) -> bool:
+		# only show keyboard input
+		return context.input.device_type & GUIDEInput.DeviceType.KEYBOARD > 0
+
+func _filter_joy() -> void:
+	_formatter.formatting_options.input_filter = func(context:GUIDEInputFormatter.FormattingContext) -> bool:
+		# only show keyboard input
+		return context.input.device_type & GUIDEInput.DeviceType.JOY > 0
+	
+func test_simple_input() -> void:
+	map(_context, _action, input_key(KEY_A))
+	GUIDE.enable_mapping_context(_context)
+	
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[A]")
+	
+	
+func test_multi_binding() -> void:
+	map(_context, _action, input_key(KEY_A))
+	map(_context, _action, input_key(KEY_B))
+	
+	GUIDE.enable_mapping_context(_context)
+	
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[A], [B]")
+
+func test_combo() -> void:
+	var combo_a := action_bool()
+	var combo_b := action_bool()
+	map(_context, combo_a, input_key(KEY_A))
+	map(_context, combo_b, input_key(KEY_B))
+	map(_context, _action, null, [], [trigger_combo([combo_a, combo_b])])
+
+	GUIDE.enable_mapping_context(_context)
+		
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[A] > [B]")
+	
+func test_chorded() -> void:
+	var chord := action_bool()
+	map(_context, chord, input_key(KEY_A))
+	map(_context, _action, input_key(KEY_B), [], [trigger_chorded_action(chord)])
+	
+	GUIDE.enable_mapping_context(_context)
+	
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[A] + [B]")
+	
+	
+func test_type_filter_simple() -> void:
+	map(_context, _action, input_key(KEY_A))
+	map(_context, _action, input_joy_button(JOY_BUTTON_B))
+	GUIDE.enable_mapping_context(_context)
+	
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[A], [Joy 1]")
+	
+	_filter_keyboard()
+	
+	result = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[A]")
+
+	_filter_joy()
+	
+	result = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[Joy 1]")
+
+
+func test_type_filter_chord() -> void:
+	var chord := action_bool()
+	map(_context, chord, input_key(KEY_SHIFT))
+	map(_context, chord, input_joy_button(JOY_BUTTON_LEFT_SHOULDER))
+	
+	
+	map(_context, _action, input_key(KEY_A), [], [trigger_chorded_action(chord)])
+	map(_context, _action, input_joy_button(JOY_BUTTON_B), [], [trigger_chorded_action(chord)])
+	GUIDE.enable_mapping_context(_context)
+	
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[Shift], [Joy 9] + [A], [Shift], [Joy 9] + [Joy 1]")
+	
+	_filter_keyboard()
+	
+	result = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[Shift] + [A]")
+
+	_filter_joy()
+	
+	result = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[Joy 9] + [Joy 1]")
+
+
+
+func test_type_filter_empty() -> void:
+	map(_context, _action, input_key(KEY_A))
+	GUIDE.enable_mapping_context(_context)
+
+	_filter_joy()
+	
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("")

tests/guide_test_base.gd
@@ -202,6 +202,13 @@ func trigger_chorded_action(action:GUIDEAction) -> GUIDETriggerChordedAction:
 	result.action = action
 	return result
 		
+func trigger_combo(steps:Array[GUIDEAction]) -> GUIDETriggerCombo:
+	var result := GUIDETriggerCombo.new()
+	for step:GUIDEAction in steps:
+		var combo_step := GUIDETriggerComboStep.new()
+		combo_step.action = step
+		result.steps.append(combo_step)
+	return result
 		
 @warning_ignore("shadowed_variable")
 func map(context:GUIDEMappingContext, action:GUIDEAction, input:GUIDEInput, \

commit c2088189dfe0b14fbb9e6ef5299597219883af9c

fix: chord-only mappings render nothing in the input formatter
_materialize_action_input() only appended the materialized chord when the
input mapping also had an own input, so a mapping whose triggers are all
chorded actions (e.g. "stop" = "left" + "right") rendered an empty prompt.
This regressed with c9c63a2 (the fix for #175), which nested the append
inside the own-input branch.

The chord is now always appended; the #175 discard (own input present but
filtered out, so the mapping can never trigger) is kept as an early
continue.

addons/guide/ui/guide_input_formatter.gd
@@ -278,15 +278,17 @@ func _materialize_action_input(action:GUIDEAction) -> MaterializedInput:
 				chord.parts.append(chorded_action)
 			for combo in combos:
 				chord.parts.append(combo)
-			if combos.is_empty():
-				if input_mapping.input != null:
-					var additional_inputs := _materialize_input(FormattingContext.for_action(input_mapping.input, input_mapping, action))
-					# https://github.com/godotneers/G.U.I.D.E/issues/175
-					# additional inputs can be blank if they are filtered out. if they are blank
-					# we can discard the whole input mapping, as it could never trigger (we have a chorded action + no input)
-					if not additional_inputs.is_blank():
-						chord.parts.append(additional_inputs)					
-						result.parts.append(chord)
+			if combos.is_empty() and input_mapping.input != null:
+				var additional_inputs := _materialize_input(FormattingContext.for_action(input_mapping.input, input_mapping, action))
+				# https://github.com/godotneers/G.U.I.D.E/issues/175
+				# additional inputs can be blank if they are filtered out. if they are blank
+				# we can discard the whole input mapping, as it could never trigger (we have a chorded action + no input)
+				if additional_inputs.is_blank():
+					continue
+				chord.parts.append(additional_inputs)
+			# a mapping can be chorded without having an own input (e.g. "stop" = LT + RT),
+			# in which case the chord is all there is to render.
+			result.parts.append(chord)
 		else:
 			for combo in combos:
 				result.parts.append(combo)

tests/test_guide_input_formatter.gd
@@ -70,8 +70,38 @@ func test_chorded() -> void:
 	
 	var result:String = _formatter.action_as_text(_action)
 	assert_str(result).is_equal("[A] + [B]")
-	
-	
+
+
+# a mapping can consist of chorded actions only, without an own input
+# (e.g. "stop" = "left" + "right"). the chord is then all there is to render.
+func test_chorded_without_own_input() -> void:
+	var left := action_bool()
+	var right := action_bool()
+	map(_context, left, input_key(KEY_A))
+	map(_context, right, input_key(KEY_B))
+	map(_context, _action, null, [], [trigger_chorded_action(left), trigger_chorded_action(right)])
+
+	GUIDE.enable_mapping_context(_context)
+
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("[A] + [B]")
+
+
+# https://github.com/godotneers/G.U.I.D.E/issues/175 - if the mapping has an own
+# input and that input is filtered out, the whole mapping is discarded because it
+# could never trigger.
+func test_chorded_with_filtered_out_own_input_is_discarded() -> void:
+	var chord := action_bool()
+	map(_context, chord, input_key(KEY_SHIFT))
+	map(_context, _action, input_joy_button(JOY_BUTTON_B), [], [trigger_chorded_action(chord)])
+
+	GUIDE.enable_mapping_context(_context)
+	_filter_keyboard()
+
+	var result:String = _formatter.action_as_text(_action)
+	assert_str(result).is_equal("")
+
+
 func test_type_filter_simple() -> void:
 	map(_context, _action, input_key(KEY_A))
 	map(_context, _action, input_joy_button(JOY_BUTTON_B))